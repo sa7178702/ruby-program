3ea2a9ee09d245ac9cfa8716cccd843377f9ec7e
sa7178702@gmail.com <sa7178702@gmail.com>
2023-10-16 12:21:47 +0530

    Using the class and method and modifyied some condition

--- Basic/calculator.rb
+++ Basic/calculator.rb
@@ -1,56 +1,81 @@
-operations = {
+class Calculator
+  OPERATIONS = {
+    'add' => '+',
+    'sub' => '-',
+    'mul' => '*',
+    'div' => '/'
+  }
 
-	'add' => '+',
-	'sub' => '-',
-	'mul' => '*',
-	'div' => '/' 
-}
-current_result = nil
-loop do 
-	puts "Enter the operations which one you want !"
+  def initialize
+    @current_result = nil
+  end
 
-	operations.each do |key, value|
-		puts "#{key.capitalize} (#{value})"
-	end
-	puts "Enter the operations !"
-	choise = gets.chomp.downcase
+  def calculate
+    loop do
+      puts "Write a operation like (add, sub, mul, div) or to exit press q "
+      choice = gets.chomp.downcase
+      break if choice == 'q'
 
-	break if choise == 'q'
-
-	if operations.key?(choise)
-		if current_result.nil?
-      puts "Enter the first number:"
-      num1 = gets.chomp.to_f
-    else
-      puts "Use the current result #{current_result} as the second number? (y/n)"
-      # num1 = gets.chomp.downcase == 'y' ? current_result : gets.chomp.to_f
-      if gets.chomp.downcase == 'y'
-      	num1 = current_result
+      if OPERATIONS.key?(choice)
+        num1 = get_first_number
+        num2 = get_second_number
+        @current_result = calculate_result(choice, num1, num2)
+        check_integer_float_result
       else
-      	puts "Enter the first number : "
-      	num1 = gets.chomp.to_f
+        puts "Invalid operation. Please enter a valid one."
       end
     end
+  end
 
-		puts "Enter the second number : "
-		num2 = gets.chomp.to_f
+  private
 
-		current_result = case choise
-			when 'add'
-				num1 + num2
-			when 'sub'
-				num1 - num2
-			when 'mul'
-				num1 * num2
-			when 'div'
-				if choise == 0
-					puts "Zero is not divisible "
-				else
-					num1 + num2
-				end
-			end
-		puts "current result : #{current_result}"
-	else
-		puts "Invalid operations Please enter valid one ?"
-	end
+  def get_first_number
+    if @current_result.nil?
+      puts "Enter the first number:"
+      gets.chomp.to_f
+    else
+      puts "Use the current result #{@current_result} as the second number? (y/n)"
+      choice = gets.chomp.downcase
+      choice == 'y' ? @current_result : get_user_input("Enter the first number:").to_f
+    end
+  end
+
+  def get_user_input(prompt)
+    puts prompt
+    gets.chomp
+  end
+
+  def get_second_number
+    get_user_input("Enter the second number:").to_f
+  end
+
+  def calculate_result(operation, num1, num2)
+    case operation
+    when 'add'
+      num1 + num2
+    when 'sub'
+      num1 - num2
+    when 'mul'
+      num1 * num2
+    when 'div'
+      num2.zero? ? (puts "Zero is not divisible"; nil) : num1 / num2
+    end
+  end
+
+  def check_integer_float_result
+  	if @current_result.is_a?(Integer)
+	    puts "The result is a: #{@current_result}"
+	  elsif @current_result.is_a?(Float)
+	    puts "The result is a : #{@current_result}"
+	  else
+	    puts "The result is not a valid number."
+	  end
+	  @current_result
+  end
 end
+
+calculator = Calculator.new
+calculator.calculate
+
+
+
